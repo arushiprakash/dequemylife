1005195ea9590cc595ce87159e854d252db3ef4d
karandaftary <karandaftary@gmail.com>
2018-09-10 09:54:09 -0700

    Adding testing framework = JUnit 5

---
+++ src/QueueTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void enqueue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dequeue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printQueue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void peek() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSize() {
+    }
+}
